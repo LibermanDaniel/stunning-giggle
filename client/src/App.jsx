5fa2b8451080356c1fb4d8e88882288f215589ae
xiaos <jessie.ninomiya@outlook.com>
2022-09-20 21:28:38 +0300

    login and signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Login from './pages/login'
+import Registration from './pages/registration'
+
 
 function App() {
   return (
+
     <div className>
       <header className>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Login/>
+        <Registration/>
         <a
           className="App-link"
           href="https://reactjs.org"
